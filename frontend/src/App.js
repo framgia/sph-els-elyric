736e4b58c0700569a8523b8ed95c4b8167e9f5a4
Elyric Manatad <elyric.manatad@sun-asterisk.com>
2023-01-06 11:32:34 +0800

    Registration Page UI Markup 02.2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,18 +3,18 @@ import RegistrationPage from "./pages/RegistrationPage";
 import HeaderNav from "./components/HeaderNav";
 
 function App() {
-	return (
-		<div>
-			<div className="px-10 py-5 bg-gradient-to-b from-[#617EFF] to-[#34B3F9] text-gray-50">
-				<HeaderNav />
-			</div>
-			<div className="mx-20">
-				<Routes>
-					<Route path="/register" element={<RegistrationPage />} />
-				</Routes>
-			</div>
-		</div>
-	);
+    return (
+        <div>
+            <div className="px-10 py-5 bg-gradient-to-b from-[#617EFF] to-[#34B3F9] text-gray-50">
+                <HeaderNav />
+            </div>
+            <div className="mx-20">
+                <Routes>
+                    <Route path="/register" element={<RegistrationPage />} />
+                </Routes>
+            </div>
+        </div>
+    );
 }
 
 export default App;
--- frontend/src/pages/RegistrationPage.js
+++ frontend/src/pages/RegistrationPage.js
@@ -1,9 +1,9 @@
 import RegistrationForm from "../components/RegistrationForm";
 
 export default function RegistrationPage() {
-	return (
-		<div className="w-full h-screen grid place-items-center">
-			<RegistrationForm />
-		</div>
-	);
+    return (
+        <div className="w-full h-screen grid place-items-center">
+            <RegistrationForm />
+        </div>
+    );
 }
