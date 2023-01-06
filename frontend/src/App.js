4cb323c52a9bd3389084bdde83c2514ce22d2e74
Elyric Manatad <elyric.manatad@sun-asterisk.com>
2023-01-06 11:23:54 +0800

    Revert "Registration Page UI Markup 02"
    This reverts commit c6f0c0b8c53a06e7f81cd3128eb9145b68ba717a.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,25 @@
-import { Route, Routes } from "react-router-dom";
-import RegistrationPage from "./pages/RegistrationPage";
-import HeaderNav from "./components/HeaderNav";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-	return (
-		<div>
-			<div className="px-10 py-5 bg-gradient-to-b from-[#617EFF] to-[#34B3F9] text-gray-50">
-				<HeaderNav />
-			</div>
-			<div className="mx-20">
-				<Routes>
-					<Route path="/register" element={<RegistrationPage />} />
-				</Routes>
-			</div>
-		</div>
-	);
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
--- frontend/src/pages/RegistrationPage.js
+++
@@ -1,9 +0,0 @@
-import RegistrationForm from "../components/RegistrationForm";
-
-export default function RegistrationPage() {
-	return (
-		<div className="w-full h-screen grid place-items-center">
-			<RegistrationForm />
-		</div>
-	);
-}
