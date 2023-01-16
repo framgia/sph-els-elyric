94612887587547213932f5ff0eeb3cd339c0c953
Elyric Manatad <elyric.manatad@sun-asterisk.com>
2023-01-16 17:21:04 +0800

    Fixed Code Indentation

--- backend/app/Http/Controllers/UserController.php
+++ backend/app/Http/Controllers/UserController.php
@@ -9,8 +9,8 @@ use Auth;
 
 class UserController extends Controller
 {
-    public function register(Request $request){
-
+    public function register(Request $request)
+	{
 		$request->validate([
 			'name' => 'required|string',
     		'email' => 'required|string|email|unique:users',
@@ -29,40 +29,43 @@ class UserController extends Controller
 		]);
 	}
 
-
-	public function login(Request $request){
-	$request->validate([
-        'email' => 'required|email',
-        'password' => 'required'
-    ]);
+	public function login(Request $request)
+	{
+		$request->validate([
+    	    'email' => 'required|email',
+    	    'password' => 'required'
+    	]);
 	
-    $user = User::where('email', $request->email)->first();
-    if (! $user || ! Hash::check($request->password, $user->password)) {
-        return response()->json(['message' => 'Invalid email or password'], 401);
-    }
+    	$user = User::where('email', $request->email)->first();
+		
+    	if (! $user || ! Hash::check($request->password, $user->password)){
+    	    return response()->json(['message' => 'Invalid email or password'], 401);
+    	}
 
-    if(Auth::guard('user')->attempt(['email' => $request->email, 'password' => $request->password])){
-		$user = Auth::guard('user')->user();
-		$token = $user->createToken('MyApp', ['user'])->plainTextToken;
-		return response()->json([
-			'message' => 'User Successfully Authenticated!',
-			'token' => $token,
-			'redirect' => 'user-dashboard'
-		]);
+    	if(Auth::guard('user')->attempt(['email' => $request->email, 'password' => $request->password])){
+			$user = Auth::guard('user')->user();
+			$token = $user->createToken('MyApp', ['user'])->plainTextToken;
+			return response()->json([
+				'message' => 'User Successfully Authenticated!',
+				'token' => $token,
+				'redirect' => 'user-dashboard'
+			]);
+		}
 	}
-}
 
-	public function logout(Request $request){
-    if($request->user()){
-        $request->user()->currentAccessToken()->delete();
-        return response()->json([
-            'message' => 'User Successfully Logged out!'
-        ]);
-    }
-    return response()->json(['error' => 'Unauthenticated'], 401);
-}
+	public function logout(Request $request)
+	{
+    	if($request->user()){
+    	    $request->user()->currentAccessToken()->delete();
+    	    return response()->json([
+    	        'message' => 'User Successfully Logged out!'
+    	    ]);
+    	}
+    	return response()->json(['error' => 'Unauthenticated'], 401);
+	}
 
-	public function userDetails(){
+	public function userDetails()
+	{
 		$user = Auth::user();
 		return response()->json(['data' => $user]);
 	}
