48e8010868a60f0f5660ee3e465660185edf3ee6
Elyric-Sun* <110363852+elyric-sun-asterisk-dev@users.noreply.github.com>
2023-01-26 16:43:30 +0800

    Feature/admin categories UI markup (#11)
    * Removing Role Column on User and Admin
    
    * Fixed Code Indentation
    
    * Login-Integration-1
    
    * Login-Integration-2 with fixed comments and adjustments
    
    * Login-Integration-2 with fixed comments and adjustments-2
    
    * fixed logout base on comments
    
    * Admin Categories Page UI Markup
    
    * Deleted unused files (Windows Environment Started here)

---
+++ frontend/src/components/adminLogout.js
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom";
+import { adminLogout } from "../api/api";
+
+function useAdminLogout({ inActiveClass }) {
+    const handleLogout = async () => {
+        await adminLogout();
+
+        localStorage.removeItem("admin_token");
+        localStorage.removeItem("isAdmin");
+        window.location.href = "/admin/login";
+    };
+    return (
+        <div>
+            <Link className={inActiveClass} onClick={handleLogout}>
+                Logout
+            </Link>
+        </div>
+    );
+}
+
+export default useAdminLogout;
