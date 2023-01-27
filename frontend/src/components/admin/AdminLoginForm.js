d6ca2582f7353922bdac31faaded94e7d8831551
Elyric Manatad <elyric.manatad@sun-asterisk.com>
2023-01-27 11:20:58 +0800

    Login-Integration-2 with fixed comments and adjustments-3

--- frontend/src/components/admin/AdminLoginForm.js
+++ frontend/src/components/admin/AdminLoginForm.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import { useToast } from "../../hooks/useToast";
 import { loginAdmin } from "../../api/api";
